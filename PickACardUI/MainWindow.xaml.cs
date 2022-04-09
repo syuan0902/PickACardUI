5bd687159911432a48a5e1ed114a1faaa5244f33
syuan7492 <kurosaki7492@gmail.com>
2022-04-09 22:39:27 +0800

    加上2行註解

--- PickACardUI/MainWindow.xaml.cs
+++ PickACardUI/MainWindow.xaml.cs
@@ -27,8 +27,12 @@ namespace PickACardUI
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            //將CardPick類別宣告成新實例
             CardPicker m_cardPicker = new CardPicker();
+
+            //將Slider的值(Double)轉換成int，才能被PickSomeCards方法使用
             string[] pickedCards = m_cardPicker.PickSomeCards((int)numberOfCards.Value);
+
             listOfCards.Items.Clear();
             foreach (string card in pickedCards)
             {
